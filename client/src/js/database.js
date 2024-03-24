67da576df2d8332a495ea04ef3de7e3c28f5d62d
salidamaharjan <salidamaharjan@outlook.com>
2024-03-24 17:42:33 -0500

    add logic to edit the content of text

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,19 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error("putDb not implemented");
+export const putDb = async (id, content) => {
+  try {
+    console.log("PUT to the database");
+    const jateDb = await openDB("jate", 1);
+    const text = jateDb.transaction("jate", "readwrite");
+    const store = text.objectStore("jate");
+    const request = store.put({ id: id, value: content });
+    const result = await request;
+    console.log("Data saved to the database", result);
+  } catch (error) {
+    console.error("putDb not implemented");
+  }
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
