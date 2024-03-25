3dbdc9b170242797859cae3ad72c79db43697ba5
salidamaharjan <salidamaharjan@outlook.com>
2024-03-25 09:52:08 -0500

    code reviewed in database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,17 +13,17 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (id, content) => {
+export const putDb = async (content) => {
   try {
     console.log("PUT to the database");
     const jateDb = await openDB("jate", 1);
     const text = jateDb.transaction("jate", "readwrite");
     const store = text.objectStore("jate");
-    const request = store.put({ id: id, value: content });
+    const request = store.put({ id: 1, content });
     const result = await request;
     console.log("Data saved to the database", result);
   } catch (error) {
-    console.error("putDb not implemented");
+    console.error("Error occurred", error);
   }
 };
 
@@ -34,12 +34,12 @@ export const getDb = async () => {
     const jateDB = await openDB("jate", 1);
     const text = jateDB.transaction("jate", "readonly");
     const store = text.objectStore("jate");
-    const request = store.getAll();
+    const request = store.get(1);
     const result = await request;
     console.log("result.value", result);
-    return result;
+    return result?.content ?? undefined;
   } catch (error) {
-    console.error("getDb not implemented");
+    console.error("Error occurred", error);
   }
 };
 
